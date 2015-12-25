0b879160c24f2eab94caa4ac5d0c25dc390ed480
swyoon <bluerumex@naver.com>
2015-12-25 20:04:57 +0900

    * 회원가입 진행중..

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
+public class UserController {
+
+    public void userJoin(HttpServletRequest request, HttpServletResponse response, HttpSession session, Model model) {
+        
+    }
+}
